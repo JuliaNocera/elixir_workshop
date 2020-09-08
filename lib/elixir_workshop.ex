cec90ea10bb767946415dcd914ddf272d88e458f
Jerel Miller <jmiller@newrelic.com>
2020-09-08 12:45:09 -0700

    Initial commit

---
+++ lib/elixir_workshop.ex
@@ -0,0 +1,18 @@
+defmodule ElixirWorkshop do
+  @moduledoc """
+  Documentation for `ElixirWorkshop`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirWorkshop.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
